8cbc597a7821ffa0e01b5e6cf8e97160ce58e75c
luiscg01 <luiscg0101@gmail.com>
2023-01-12 10:03:40 +0100

    Listfiles a partir de Lista estatica

--- lib/screens/listview1_screen.dart
+++ lib/screens/listview1_screen.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class Listview1Screen extends StatelessWidget {
    
   const Listview1Screen({Key? key}) : super(key: key);
-  
+  final options = const ['Megaman', 'Metal Gear', 'Super Smash', 'final fantasy'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,13 +11,13 @@ class Listview1Screen extends StatelessWidget {
         title: const Text('ListView tipo 1'),
       ),
       body: ListView(
-        children: const [
-          Text('HOLA MUNDO'),
-          Text('HOLA MUNDO'),
-          Text('HOLA MUNDO'),
-          Text('HOLA MUNDO'),
-          Text('HOLA MUNDO'),
-          Text('HOLA MUNDO'),
+        children: [
+          ...options.map((e) => 
+            ListTile(
+              trailing: const Icon(Icons.arrow_circle_down),
+              title: Text(e),
+            )
+          )
         ],
       )
     );
