d008c04a059b6c29b47ea0579f09a54b280b3e9a
luiscg01 <120457538+luiscg01@users.noreply.github.com>
2023-01-16 11:33:02 +0100

    Nvegacion a una nueva Pantalla

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,11 +8,22 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       AppBar: AppBar{
-          title
+          title: const Text('Componentes en Flutter'),
+          elevation: 0,
       }
-      body: Center(
-         child: Text('HomeScreen'),
-      ),
+      body: ListView.separeted(
+        itemBuilder: (context, index) => ListTittle(
+          title: Text(),
+          leading: Icon(Icons.access_time_outlined),
+          title: const Text(),
+          onTap: () {
+            final route = MaterialPageRoute(builder: (context) => ListviewScreen());
+            Navigator.push(context, route);
+          },
+        )
+        separatorBuilder: (_,_) => const Builder(),
+        itemCount: 100  
+      )
     );
   }
 }
