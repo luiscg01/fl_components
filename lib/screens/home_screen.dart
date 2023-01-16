4a9e6a952f50de8e26964ef608efbf65e17df107
luiscg01 <120457538+luiscg01@users.noreply.github.com>
2023-01-16 11:26:58 +0100

    Diseño del HomeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,6 +7,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      AppBar: AppBar{
+          title
+      }
       body: Center(
          child: Text('HomeScreen'),
       ),
