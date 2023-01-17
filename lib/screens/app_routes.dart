7931928ff3bcd154ca8f228450ae0ea74a949371
luiscg01 <120457538+luiscg01@users.noreply.github.com>
2023-01-17 10:59:58 +0100

    Moduelo y listado de Rutas

--- lib/screens/app_routes.dart
+++ lib/screens/app_routes.dart
@@ -1,3 +1,25 @@
 class AppRoutes {
   static const initialRoute = 'home'
+
+  static final menuOptions = <MenuOption>{
+    //TODO: borrar home
+    MenuOption(route: 'home', name: 'HomeScreen',   screen: const HomeScreen(), icon: Icons.home_max_sharp),
+    MenuOption(route: 'listview1', name: 'listview tipo 1',   screen: const listviewScreen(), icon: Icons.home_max_sharp),
+    MenuOption(route: 'listview2', name: 'listview tipo 2',   screen: const listview1Screen(), icon: Icons.home_max_sharp),
+    MenuOption(route: 'alert', name: 'Alertas',   screen: const AlertScreen(), icon: Icons.home_max_sharp),
+    MenuOption(route: 'card', name: 'Tarjetas - Cards ',   screen: const CardScreen(), icon: Icons.home_max_sharp),
+  };
+
+  static Map<String, Widget function(BuildContext)> routes = {
+        'home':  (BuildContext context) => const HomeScreen(),
+        'listview1':  (BuildContext context) => const Listview1Screen(),
+        'listview2':  (BuildContext context) => const Listview2Screen(),
+        'alert':  (BuildContext context) => const AlertScreen(),
+        'card':  (BuildContext context) => const CardScreen(),
+  };
+
+  static Route<dynamic> onGenerateRoute<dynamic> (settings){
+        return  MaterialPageRoute (
+          builder: (context) =>  const AlertScreen(),
+        );  
 }
