b6bb90f85a033d24920a488ac9fe76d858cead3f
luiscg01 <120457538+luiscg01@users.noreply.github.com>
2023-01-19 10:43:43 +0100

    CardWidget

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -7,9 +7,26 @@ class CardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-         child: Text('CardScreen'),
-      ),
+      appBar: AppBar{
+        tittle: const Text('Card Widget'),
+      },  
+      body: ListView( 
+          padding: const EdgeInsets.symetric(horizontal: 20, vertical: 30),
+          children: const [
+            Card(
+              child: Colum (
+                children: const [
+                  ListTile(
+                    leading: Icon(Icons.photo_album_outlined, color: AppTheme.primary),
+                    title: Text('Soy un titulo'),
+                    subtitle: Text('Soy un subtitulo'),
+                  )
+                ],
+              )
+            )
+
+          ],
+      )
     );
   }
 }
