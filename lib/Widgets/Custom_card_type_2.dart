631bab6a04651d493b634c67e1d3621fdf60a3b0
luiscg01 <120457538+luiscg01@users.noreply.github.com>
2023-01-20 10:44:00 +0100

    Ajustar el tamaño de la Imagen

--- lib/Widgets/Custom_card_type_2.dart
+++ lib/Widgets/Custom_card_type_2.dart
@@ -5,12 +5,26 @@ class CustomCardType2 extends StatelesWidget {
 
   Widget build (BuildContext context){
     return Card(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(
+        borderRadius.circular(15)
+      )
       elevation: 30,
       Child: Column(
         children: const [
           FadeInImage(
             image: const NetworkImage('https://www.noegasystems.com/wp-content/uploads/metodo-fifo.jpg')
             placeholder: AssetImage('assets/jar-loading.git'),
+            width: double.infinity,
+            height: 230,
+            fit: Boxfit.cover,
+            fadeInDuration: Duration( milliseconds: 300),
+          ),
+
+          container(
+            alignment: AlignmentDirectional.centerEnd,
+            padding: EdgeInserts.only(right:20, top:10, button: 10),
+            child: Text('Un hermoso paisaje'),
           )
         ],
       )
