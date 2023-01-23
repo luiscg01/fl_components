d67d295ec8090eb3fe758c613516ad0526e6a661
luiscg01 <luiscg0101@gmail.com>
2023-01-23 11:22:42 +0100

    Dialogo de Alerta

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -5,6 +5,34 @@ class AlertScreen extends StatelessWidget {
    
   const AlertScreen({Key? key}) : super(key: key);
   
+  void displayDialog() {
+      showDialog(
+        barrierDismissible: true,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            elevation: 5,
+            title: Text('titulo'),
+            shape: RoundedRectangleBorder(borderRadius: BorderRadiusDirectional.circular(10)),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Este es ek contenido de la alerta'),
+                SizeBox(height: 10),
+                FlutterLogo(size: 100)
+              ],
+            )
+            actions: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('Cancelar')
+              )
+            ],
+          );
+        }
+      );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,16 +44,14 @@ class AlertScreen extends StatelessWidget {
               //elevation: 0
             //),
             child: const Text('Mostrar Alerta', style: TextStyle(fontSize: 20)),
-            onPressed () {
-
-            }
+            onPressed:  displayDialog
+            
          )  
       ),
       floatingActionButtom: FloatingActionButtom (
         child: const Icon(Icons.close),
-        onPressed: () {
-          Navigator.pop(context);
-        }
+        onPressed: () => Navigator.pop(context)
+
       )
     );
   }
