be22990be1dc0493a5beb3aa63cefb61e0953f19
luiscg01 <luiscg0101@gmail.com>
2023-01-25 12:50:19 +0100

    TextFormFields Personalizacion

--- lib/screens/input_screen.dart
+++ lib/screens/input_screen.dart
@@ -17,9 +17,25 @@ class inputScreen extends StatefullsWidget {
                                 initialValue: 'Fernando Herrera',
                                 textCapitalization: TextCapitalization.words,
                                 onChanged: (value) {
-                                    
-                                }
-
+                                    print('value: $value');
+                                },
+                                validator:(value) {
+                                    if(value == null) {
+                                        return 'Este campo es requerido';
+                                        return value.length <3 ?  'Minimo de 3 letras' : null;
+                                    },
+                                    autoValidateMode: AutoValidateMode.onUserInteraction,
+                                    decoration: InputDecoration(
+                                        hintText: 'Nombre del Usuario',
+                                        labelText:'Nombre',
+                                        helperText: 'Solo letras', 
+                                        counterText: '3 caracteres',
+                                        suffixIcon: Icon(Icons.group_outlined),
+                                        border: OutlineInputBorder(
+                                            borderRadius: BorderRadius.only(bottomLeft: Radius.circular(10))            
+                                        )
+                                    )
+                                },
                             }
                         ]
                     )
