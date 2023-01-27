2fa924264905a89845f925cfc1aabc3bf22ef41b
luiscg01 <luiscg0101@gmail.com>
2023-01-27 12:03:30 +0100

    Widget Form

--- lib/screens/input_screen.dart
+++ lib/screens/input_screen.dart
@@ -3,6 +3,16 @@ class inputScreen extends StatefullsWidget {
     const inputScreen{key? key}: super (key : key);
 
     Widget build (BuildContext context){
+        final GlobalKey<FormState> myFormKey = GlobalKey<FormState>();
+
+        final Map<String, String>  formValues = {
+            'first name': 'fernando',
+            'last name': 'herrera',
+            'email': 'fernando@google.com',
+            'password': '123456',
+            'user': 'admin',
+
+        }
         return Scaffold(
             appBar: AppBar(
                 title: Text ('Inputs y Forms'),
@@ -10,20 +20,37 @@ class inputScreen extends StatefullsWidget {
             body: SingleChildScrollView(
                child: Padding(
                     padding: const EdgeInserts.symetric(horizontal: 20, vertical: 10),
-                    child: Column(
-                        children: [
-                           CustomInputField(labelText: 'Nombre', hintText: 'Nombre de Usuario')
-                           SizeBox(height: 30)
+                    child: form(
+                        key: myFormKey,
+                        child: Column(
+                            children: [
+                            CustomInputField(labelText: 'Nombre', hintText: 'Nombre de Usuario')
+                            SizeBox(height: 30)
 
-                          CustomInputField(labelText: 'Apellido', hintText: 'Apellido de Usuario')
-                           SizeBox(height: 30)
+                            CustomInputField(labelText: 'Apellido', hintText: 'Apellido de Usuario')
+                            SizeBox(height: 30)
 
-                           CustomInputField(labelText: 'Correo', hintText: 'Correo de Usuario', keyboardType: TextInputType.emailAddress,)
-                           SizeBox(height: 30)
+                            CustomInputField(labelText: 'Correo', hintText: 'Correo de Usuario', keyboardType: TextInputType.emailAddress,)
+                            SizeBox(height: 30)
 
-                           CustomInputField(labelText: 'Contraseña', hintText: 'Contraseña de Usuario', obscureText: true)
-                           SizeBox(height: 30)
-                        ]
+                            CustomInputField(labelText: 'Contraseña', hintText: 'Contraseña de Usuario', obscureText: true)
+                            SizeBox(height: 30)
+
+                            ElevatedBotton (
+                                  child const SizeBox(
+                                  width: double.infinity,
+                                  child: Text('Guardar'),
+                                )
+                                onPressed() {
+                                    FocusScope.of(context).requestFocus(FocusNode());
+                                    if(!myFormKey.currentState.validate()) {
+
+                                    }
+                                }
+                            )
+                    
+                            ]
+                        )
                     )
                ),
             )
