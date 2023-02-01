4fd6325b07e76346f930006161379d418cd86f03
luiscg01 <luiscg0101@gmail.com>
2023-02-01 11:42:28 +0100

    Infinte Scroll con Futures

--- lib/screens/listview_builder_screen.dart
+++ lib/screens/listview_builder_screen.dart
@@ -6,21 +6,34 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen>{
 
   final List <int> imagesIds = [1,2,3,4,5,6,7,8,9,10]
   final ScrollControler scrollControler = new ScrollControler();
-
+  bool isLoading = false;
   void initState() {
     super.initState();
-    scrollControler.addListener((){
-        print('')
-    });
+    scrollControler.addListener(() {
+          if(scrollControler.position.pixels + 500) <= scrollControler.position.maxScrollExent) {
+            fetchData();
+          }
+      });
+    }
+  }
+
+  Future fetchData()  async{
+    if(isLoading) return;
+    isLoading = true;
+    setState(() {})
+
+    await Future.delayed(const Duration(seconds: 3))
+    add5();
+    isLoading = false;
+    setState(() {})
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-     body MediaQuery.removePadding(
-      removeTop: true,
-      removeBottom: true,
-      child: ListView.builder (
+    return const Container(
+      child: Stack(
+      children:  [
+      ListView.builder (
         physics: const BouncingScrollPhysics(),
         controller: scrollControler,
         itemCount:imagesIds.length,
@@ -34,8 +47,20 @@ class _ListViewBuilderScreenState extends State<ListViewBuilderScreen>{
             );
         },
       ),
-     ),
-    );
+
+      Container(
+        color: Colors.white,
+        height: 60,
+        width: 60,
+        child: CircularProgresIndicator(color: AppTheme.primary),
+        padding: EdgeInsets.al(10),
+        decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.9),
+          shape: BoxShape.ciircle
+        )
+      )
+    ],
+    ),
+    ),
   }
-}
 }
